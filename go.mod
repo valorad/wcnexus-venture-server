070e5d600ccbfc43997ee8fcd59edd90fb9268a7
valorad <valorad@outlook.com>
2020-12-28 14:43:04 -0500

    add go-graphql-client

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/valorad/wcnexus-venture-server
 
-go 1.13
+go 1.15
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
+	github.com/hasura/go-graphql-client v0.2.0
 	github.com/labstack/echo/v4 v4.1.14
 )
--- src/routes/index.go
+++ src/routes/index.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/hasura/go-graphql-client"
 	"github.com/labstack/echo/v4"
 )
 
@@ -20,9 +23,34 @@ func IndexRoutes(group *echo.Group) {
 	group.GET("/a", Index)
 }
 
+func GetTheme(c echo.Context) error {
+
+	client := graphql.NewClient("https://example.com/graphql", nil)
+
+	var query struct {
+		Api struct {
+			Name graphql.String
+		}
+	}
+
+	err := client.Query(context.Background(), &query, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return c.String(http.StatusOK, string(query.Api.Name))
+
+}
+
+func ThemeRoutes(group *echo.Group) {
+	group.GET("/thm", GetTheme)
+}
+
 func ActivateIndex(g *echo.Group) {
 	mainGroup = g
 	UseRoute(mainGroup, IndexRoutes)
+	UseRoute(mainGroup, ThemeRoutes)
 
 	// sub routes
 
